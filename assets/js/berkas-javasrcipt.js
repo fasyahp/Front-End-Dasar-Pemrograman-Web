d5b838593d259dff7f664e3e083978a30905d45f
fasyahpgithubio <fasyapratama887@gmail.com>
2021-08-13 23:59:12 +0700

    Front End Dasar Pemrograman Web

---
+++ assets/js/berkas-javasrcipt.js
@@ -0,0 +1,40 @@
+//====== Makanan ========
+function myFunction() {
+  alert("Roti bakar dengan topping coklat dan parutan keju");
+}
+function myFunction1() {
+  alert("Dessert isinya krim, kue bolu, pudding dan topping biskuat");
+}
+function myFunction2() {
+  alert("Pisang bakar dengan topping susu coklat dan parutan keju");
+}
+function myFunction3() {
+  alert("Macaron adalah kue yang dibuat dari tepung almond, putih telur, dan gula. Diproses dengan cara dipanggang hingga menghasilkan tekstur luar yang tipis dan bagian dalam yang lembut.");
+}
+function myFunction4() {
+  alert("Bolu kukus rasa taro dan pisang diatas topping parutan keju");
+}
+function myFunction5() {
+  alert("Martabak manis dibuat dengan menggunakan tepung yang diolesi mentega, dengan ditaburti kacang, meses coklat, dan parutan keju.");
+}
+
+//======== Minuman ======
+
+function myFunction6() {
+  alert("Jus mangga adalah jus yang terbuat dari bahan dasar buah mangga");
+}
+function myFunction7() {
+  alert("Es krim merupakan produk olahan susu yang dibuat dengan cara membekukan dan mencampur bahan baku secara bersama-sama. Es krim ada 3 rasa yaitu coklat, stroberi dan vanilla");
+}
+function myFunction8() {
+  alert("Milkshake adalah minuman dingin dari campuran susu, es krim dan sirup berperasa yang dikocok dengan blender");
+}
+function myFunction9() {
+  alert("Es Teler adalah minuman es berisi potongan buah alpukat, kelapa muda, nangka matang, dan santan kelapa encer dengan pemanis berupa sirup.");
+}
+function myFunction10() {
+  alert("Es campur adalah salah satu minuman khas Indonesia yang cara membuatnya dengan mencampurkan berbagai jenis bahan dalam sirop manis.");
+}
+function myFunction11() {
+  alert("Es Brown sugar adalah minuman bubble tea dengan pearls yang terbuat dari tepung tapioka");
+}
